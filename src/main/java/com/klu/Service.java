5c04792a360ae790c26ded4f8b89ab73eab92032
vihanvamsee <psupriya@kluniversity.in>
2025-08-11 15:49:25 +0530

    Update Service.java

--- src/main/java/com/klu/Service.java
+++ src/main/java/com/klu/Service.java
@@ -27,7 +27,6 @@ public class Service {
 		}
 	}
 
-	/*
 	public String deleteData(int id) {
 		if (r1.existsById(id)) {
 			r1.deleteById(id);
@@ -36,5 +35,5 @@ public class Service {
 			return "Product not found";
 		}
 	}
-	*/
+	
 }
